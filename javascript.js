ad26418f8adc94f115f6187cd2861eb94887e129
tanglero4 <tiffanyanglero94@hotmail.com>
2022-12-31 13:56:55 -0500

    fixed timer on start and looped questions

--- javascript.js
+++ javascript.js
@@ -1,52 +1,90 @@
 let time = 60;
 const count = document.getElementById(`timer`);
-setInterval(startTimer, 1000)
-
-function startTimer(){
-    const seconds= time % 60
-    count.innerHTML = `Timer: ${time}`
-    time --;
-    if(time === 0){
-       showHighScores();
+// setInterval(startTimer, 1000)
+let timeInterval;
+function startTimer() {
+  count.innerHTML = `Timer: ${time}`;
+  timeInterval = setInterval(function () {
+    time--;
+    count.innerHTML = `Timer: ${time}`;
+    if (time <= 0) {
+      clearInterval(timeInterval);
+      time = 0;
+      count.innerHTML = `Timer: ${time}`;
+      showHighScores();
     }
+  }, 1000);
 }
 
-let question={
-    title: `Will this work?`,
-    possibleAnswers:[ `A: Yes`, `B: No`, `C: Maybe`, `D: So`],
-    correctAnswer: 2
+let question = [
+  {
+    title: `Arrays in Javascript can be used to store _____.`,
+    possibleAnswers: [
+      `A: numbers and strings`,
+      `B: other arrays`,
+      `C: booleans`,
+      `D: All of the above`,
+    ],
+    correctAnswer: 3,
+  },
+  {
+    title: ` String values must be enclosed within ____ when being assigned to variables.`,
+    possibleAnswers: [
+      `A: commas`,
+      `B: curly brakets`,
+      `C: quotes`,
+      `D: parenthesis`,
+    ],
+    correctAnswer: 2,
+  },
+  {
+    title: ` A very useful tool used during development and debugging for printing content to the debugger is: `,
+    possibleAnswers: [
+      `A: Javascript`,
+      `B: terminal/bash`,
+      `C: for loops`,
+      `D: console.log`,
+    ],
+    correctAnswer: 3,
+  },
+];
+
+let btn = document.getElementById(`btn`);
+let instruction = document.getElementById(`hide`);
+let answerUl = document.getElementById(`answer-list`);
+let index = 0;
+function showQuestion() {
+  btn.classList.add(`hide`);
+  instruction.classList.add(`hide`);
+  startTimer();
+  console.log(index);
+  // select h1 element
+  let titleH1 = document.getElementById(`title`);
+  titleH1.textContent = question[index].title;
+  // select li to display answers
+  answerUl.innerHTML = ``;
+  for (i = 0; i < question[index].possibleAnswers.length; i++) {
+    console.log(question[index].possibleAnswers);
+    var answerbtn = document.createElement("button");
+    answerbtn.innerHTML = question[index].possibleAnswers[i];
+    console.log(answerbtn);
+    answerUl.appendChild(answerbtn);
+
+    answerbtn.onclick = checkAnswer;
+  }
 }
 
-let btn= document.getElementById(`btn`)
-let instruction= document.getElementById(`hide`)
-
-btn.addEventListener(`click`, function showQuestion(x){
-btn.classList.add(`hide`)
-instruction.classList.add(`hide`)
-
-// select h1 element
-    let titleH1= document.getElementById(`title`)
-    titleH1.textContent = question.title
-
-// select li to display answers
-    let answerli1= document.getElementById(`answer1`);
-    answerli1.textContent = question.possibleAnswers[0];
-    let answerli2= document.getElementById(`answer2`);
-    answerli2.innerHTML = question.possibleAnswers[1];
-    let answerli3= document.getElementById(`answer3`);
-    answerli3.innerHTML = question.possibleAnswers[2];
-    let answerli4= document.getElementById(`answer4`);
-    answerli4.innerHTML = question.possibleAnswers[3];
-
-// Modify h1 into the question
-    // possibleAnswers.forEach(function(element, index){
-    //     Element.textContent= x.possibleAnswers[index]
-    // })
-    // showQuestion();
-});
-
-function showHighScores(){
-    console.log(`you won!`);
+function checkAnswer() {
+  console.log(this);
+  if (question.length > index) {
+    index++;
+    showQuestion();
+  }
 }
-    // showQuestion();
-    startTimer();
+
+btn.addEventListener(`click`, showQuestion);
+
+function showHighScores() {
+  console.log(`you won!`);
+}
+// showQuestion();
