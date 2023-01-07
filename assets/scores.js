0d95f930d15776fe4dd63add568b6bbf5f2661a6
tanglero4 <tiffanyanglero94@hotmail.com>
2023-01-06 23:17:13 -0500

    Corrected High score list

--- assets/scores.js
+++ assets/scores.js
@@ -1,44 +1,25 @@
-// var userFormEl = $("#user-form");
-// var initialListEl = $("#initial-list");
-
 $(".btn").on("click", function () {
   //get user input from form
-  const userInitial = document.getElementById("user-initial").value;
+  var userInitial = $('input[name="user-initial"]').val();
 
+  // Combine initals and scores
+  const playerScore = userInitial + "..............................." + time;
+
+  // Require initials from player
+  if (!userInitial) {
+    alert("Please enter your initials");
+    return;
+  }
   //sets up local storage or make an array to add user input
-  let initials = JSON.parse(localStorage.getItem("Initials")) || [];
+  let score = JSON.parse(localStorage.getItem("Score")) || [];
 
   // save the users input to local storage
-  initials.push(userInitial);
-  localStorage.setItem("Initials", JSON.stringify(initials));
+  score.push(playerScore);
+  localStorage.setItem("Score", JSON.stringify(score));
+
+  // Add user initial and scores to the high score list
+  $("ol").append("<li>" + playerScore + "</li>");
+
+  
+  $('input[name="user-initial"]').val("");
 });
-
-// function showScores(event) {
-//   //   Prevent the default behavior
-//   event.preventDefault();
-
-//   var userInitial = $('input[name="user-initial"]').val();
-
-//   if (!userInitial) {
-//     console.log("Please enter your initials");
-//     return;
-//   }
-
-//   var initialListItemEl = $(
-//     '<li class="flex-row justify-space-between align-center p-2 bg-light text-dark ">'
-//   );
-//   initialListItemEl.text(userInitial);
-
-//   // add delete button to remove shopping list item
-//   initialListItemEl.append(
-//     '<button class="btn btn-danger btn-small delete-item-btn">Remove</button>'
-//   );
-
-//   // print to the page
-//   initialListEl.append(initialListItemEl);
-//   console.log(initialListEl);
-//   // clear the form input element
-//   $('input[name="user-initial"]').val("");
-// }
-
-// userFormEl.on("submit", showScores);
